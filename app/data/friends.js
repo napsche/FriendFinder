4fe96c3a7b0b838f8424a10df3ead560a136216f
Nicole Apsche <napsche@kgtiger.com>
2020-07-10 18:36:41 -0400

    adding friends array

---
+++ app/data/friends.js
@@ -0,0 +1,83 @@
+var friendsArray = [
+    {
+        friendName: "Kelly",
+        photo: "https://www.linkedin.com/in/kelly-dickinson-3a0237121/",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            3,
+            4,
+            3,
+            5
+        ]
+    },
+    {
+        friendName: "Chris",
+        photo: "https://www.facebook.com/photo.php?fbid=10212285940676523&set=t.1088784656&type=3",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            3,
+            4,
+            3,
+            5
+        ]
+    },
+    {
+        friendName: "Luke",
+        photo: "https://www.facebook.com/photo?fbid=10219464702181074&set=pob.8224232",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            3,
+            4,
+            3,
+            5
+        ] 
+    },
+    {
+        friendName: "Colleen",
+        photo: "https://twitter.com/sturdevacolleen",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            3,
+            4,
+            3,
+            5
+        ] 
+    },
+    {
+        friendName: "Dave",
+        photo: "https://www.facebook.com/photo?fbid=10214858782937441&set=pob.100000258512583",
+        scores: [
+            4,
+            3,
+            1,
+            2,
+            5,
+            4,
+            3,
+            4,
+            3,
+            5
+        ] 
+    }
+
+];

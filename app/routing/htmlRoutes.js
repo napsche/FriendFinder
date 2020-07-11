80552c51b0dd146d51a7dad4b11dea5c75ed8659
Nicole Apsche <napsche@kgtiger.com>
2020-07-11 17:46:09 -0400

    changing app.get to app.use

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,7 @@ app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
-app.get("*", function(req, res) {
+app.use("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 

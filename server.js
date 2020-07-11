f9a165849cc7bebbb154c7420bffe15957767914
Nicole Apsche <napsche@kgtiger.com>
2020-07-11 17:53:45 -0400

    updating server.js and gitignore

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express =  require("express");
+var bodyParser = require("body-parser");
 var path = require("path"); 
 
 var app = express();
@@ -8,9 +9,15 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/app/css"));
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);

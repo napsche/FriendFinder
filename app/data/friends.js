1dc942ef8b3bbcfe2ee5562aea8e201a51593a9c
Nicole Apsche <napsche@kgtiger.com>
2020-07-11 21:58:50 -0400

    adding css

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,88 +1,30 @@
-var friendsArray = [
+var friends = [
     {
-        friendName: "Jerry",
+        name: "Jerry",
         photo: "https://www.shutterstock.com/image-photo/los-angeles-sep-18-jerry-seinfeld-485386732",
-        scores: [
-            3,
-            2,
-            5,
-            5,
-            5,
-            2,
-            2,
-            5,
-            3,
-            4
-        ]
-        //36
+        scores: [3, 2, 5, 5, 5, 2, 2, 5, 3, 4]
     },
     {
-        friendName: "Elaine",
+        name: "Elaine",
         photo: "https://www.gettyimages.com/detail/news-photo/season-5-pictured-julia-louis-dreyfus-as-elaine-benes-news-photo/138406406?adppopup=true",
-        scores: [
-            2,
-            3,
-            4,
-            4,
-            3,
-            5,
-            2,
-            4,
-            4,
-            1
-        ]
-        //32
+        scores: [2, 3, 4, 4, 3, 5, 2, 4, 4, 1]
     },
     {
-        friendName: "Kramer",
+        name: "Kramer",
         photo: "https://www.gettyimages.com/detail/news-photo/the-van-buren-boys-episode-14-pictured-michael-richards-as-news-photo/140950316?adppopup=true",
-        scores: [
-            5,
-            4,
-            5,
-            4,
-            4,
-            4,
-            5,
-            4,
-            5,
-            4
-        ] 
+        scores: [5, 4, 5, 4, 4, 4, 5, 4, 5, 4] 
     },
     {
-        friendName: "Newman",
+        name: "Newman",
         photo: "https://www.gettyimages.com/detail/news-photo/wayne-knight-as-newman-news-photo/138428655?adppopup=true",
-        scores: [
-            1,
-            1,
-            2,
-            2,
-            2,
-            1,
-            2,
-            2,
-            5,
-            5
-        ] 
+        scores: [1, 1, 2, 2, 2, 1, 2, 2, 5, 5] 
     },
     {
-        friendName: "George",
+        name: "George",
         photo: "https://www.shutterstock.com/image-photo/jason-alexander-hollywood-celebrates-60th-anniversary-424160293",
-        scores: [
-            5,
-            5,
-            1,
-            2,
-            5,
-            1,
-            5,
-            4,
-            5,
-            5
-        ] 
-        //36
+        scores: [5, 5, 1, 2, 5, 1, 5, 4, 5, 5]
     }
 
 ];
 
-module.exports = friendsArray;
+module.exports = friends;

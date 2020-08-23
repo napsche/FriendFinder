95be15f910065f4f6826097f66477e98e74d321b
Nicole Apsche <napsche@Nicoles-MacBook-Pro.local>
2020-08-23 14:00:10 -0400

    fixing picture links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
     {
         name: "Jerry",
-        photo: "https://www.shutterstock.com/image-photo/los-angeles-sep-18-jerry-seinfeld-485386732",
+        photo: "https://i.ibb.co/0r8NPXv/jerry.jpg",
         scores: [3, 2, 5, 5, 5, 2, 2, 5, 3, 4]
     },
     {
         name: "Elaine",
-        photo: "https://www.gettyimages.com/detail/news-photo/season-5-pictured-julia-louis-dreyfus-as-elaine-benes-news-photo/138406406?adppopup=true",
+        photo: "https://i.ibb.co/Bn6hzdZ/SEINFELD-Season-5-Pictured-Julia-Louis-Dreyfus-as-Elaine-Benes-Photo-by-George-Lange-NBCU-Photo-Bank.jpg",
         scores: [2, 3, 4, 4, 3, 5, 2, 4, 4, 1]
     },
     {
         name: "Kramer",
-        photo: "https://www.gettyimages.com/detail/news-photo/the-van-buren-boys-episode-14-pictured-michael-richards-as-news-photo/140950316?adppopup=true",
+        photo: "https://i.ibb.co/0yLYGnT/SEINFELD-The-Van-Buren-Boys-Episode-14-Pictured-Michael-Richards-as-Cosmo-Kramer-Photo-by-Carin-Baer.jpg",
         scores: [5, 4, 5, 4, 4, 4, 5, 4, 5, 4] 
     },
     {
         name: "Newman",
-        photo: "https://www.gettyimages.com/detail/news-photo/wayne-knight-as-newman-news-photo/138428655?adppopup=true",
+        photo: "https://i.ibb.co/pdsxyFB/SEINFELD-Pictured-Wayne-Knight-as-Newman-Photo-by-NBCU-Photo-Bank-NBCUniversal-via-Getty-Images-via.jpg",
         scores: [1, 1, 2, 2, 2, 1, 2, 2, 5, 5] 
     },
     {
         name: "George",
-        photo: "https://www.shutterstock.com/image-photo/jason-alexander-hollywood-celebrates-60th-anniversary-424160293",
+        photo: "https://i.ibb.co/6H9G1GY/george.jpg",
         scores: [5, 5, 1, 2, 5, 1, 5, 4, 5, 5]
     }
 
